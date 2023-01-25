df5951dcf88bf535ec5bfd33f372ffd857e342fe
grachevaa01 <grachevaa01@mail.ru>
2023-01-25 17:46:17 +0300

    Сделано ДЗ, массивы урок 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,89 @@
 public class Main {
     public static void main(String[] args) {
+        int [] weights = {90,91,93,92,85,87,88,89,0,0,0,0};
+        weights [0] = 90;
+        int  januaryWeight = weights [0];
+        System.out.println(januaryWeight);
+        System.out.println(weights[0]);
+        int january = 0;
+        System.out.println(weights [january]);
+        for (int r = 0; r < weights.length; r++) {
+            System.out.println(weights[r]);
+        }
 
-    }
-}
+
+
+        int[] s = new int[4];
+        s [0] = 1;
+        int sS = s[0];
+        System.out.println(sS);
+        s [1] = 2;
+        int sSS = s [1];
+        System.out.println(sSS);
+        s [2] = 3;
+        int sSSS = s [2];
+        System.out.println(sSSS);
+        s [3] = 4;
+        int sSSSS = s [3];
+        System.out.println(sSSSS);
+
+
+
+        double [] z = {1.57, 7.654, 9.986};
+        z [0] = 1.57;
+        double zZ = z[0];
+        System.out.println(zZ);
+        z [1] = 7.654;
+        double zZZ = z [1];
+        System.out.println(zZZ);
+        z [2] = 9.986;
+        double zZZZ = z [2];
+        System.out.println(zZZZ);
+
+
+        int [] k = new int  [4];
+        k [0] = 3;
+        int kK = k [0];
+        System.out.println(kK);
+        k [1] = 2;
+        int kKK = k [1];
+        System.out.println(kKK);
+        k [2] = 1;
+        int kKKK = k [2];
+        System.out.println(kKKK);
+
+        double [] g = {9.986, 7.654, 1.57};
+        g [0] = 9.986;
+        double gG = g [0];
+        System.out.println(gG);
+        g [1] = 7.654;
+        double gGG = g [1];
+        System.out.println(gGG);
+        g [2] = 1.57;
+        double gGGG = g [2];
+        System.out.println(gGGG);
+
+        int [] n = {1,2,3,4};
+        n [0] = 1;
+        int nN = n [0];
+        System.out.println(nN);
+        n [1] = 2;
+        int nNN = n [1];
+        System.out.println(nNN);
+        n [2] = 3;
+        int nNNN = n [2];
+        System.out.println(nNNN);
+        n [3] = 4;
+        int nNNNN = n [3];
+        System.out.println(nNNNN);
+        int result = 0;
+        System.out.println(n [result]);
+        for (int t = 0; t < n.length; t++) {
+            n [t] = t +1;
+            System.out.println(t);
+
+        }
+
+
+    }//
+}//
